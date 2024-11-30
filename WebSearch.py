5d152611480a803d3e0afb54652823c84e254822
DanyDeggio <danideg12@gmail.com>
2024-11-30 23:22:38 +0100

    start

---
+++ WebSearch.py
@@ -0,0 +1,16 @@
+from toolhouse import Toolhouse
+from groq import Groq
+
+# Crea il client per Groq e Toolhouse
+client = Groq()
+th = Toolhouse()
+
+# Modello fine-tuned per tool use
+MODEL = "llama3-groq-70b-8192-tool-use-preview"
+
+# Configura il messaggio dell'utente
+messages = [
+    {888888888
+      88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+# Stampa la risposta finale dell'LLM
+print("LLM RESPONSE:", final_response.choices[0].message.content) 
